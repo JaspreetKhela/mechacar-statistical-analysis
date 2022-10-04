16e98e8dc03d204fe15fc83da2ad75f38a525e3d
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2022-10-04 15:45:47 -0400

    Finished writing the analysis script

--- mechacarchallenge.R
+++ mechacarchallenge.R
@@ -1,19 +1,52 @@
+#import dependencies
 library(dplyr)
 
-#DELIVERABLE 1
+# Deliverable 1: Linear Regression to Predict MPG
+# Import data from the mechacar_mpg.csv file
+mechacar_mpg <-read.csv(file="/Users/jaspreetkhela/Google Drive/My Drive/SCS Data Analytics Boot Camp Course Work/Boot Camp/Module 15 - Statistics and R/Application/Module 15 Challenge/mechacar-statistical-analysis/assets/data/mechacar_mpg.csv")
 
-#Import and read in the MechaCar_mpg.csv file as a dataframe
-MechaCar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-#linear regression
-linear_reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data=MechaCar_mpg_df)
-#Summary
-summary(linear_reg)
+# View the head of the created DataFrame
+head(mechacar_mpg)
 
-#DELIVERABLE 2
+# Generate multiple Regression Analysis
+lm_obj <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg)
+lm_obj
 
-#read in the Suspension_Coil.csv file
-Suspension_Coil_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-#dataframe using the summarize()
-summarize_Suspension_Coil <- Suspension_Coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep') 
-#dataframe using group_by(Manufacturing_Lot)
-summarize_SuspensionCoil_perlot <- Suspension_Coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep') 
+# Summary of the Regression Analysis
+summary(lm_obj)
+
+# Deliverable 2: Create Visualizations for the Trip Analysis
+# Import data from the suspension_coil.csv file
+suspension_coil <- read.csv(file="/Users/jaspreetkhela/Google Drive/My Drive/SCS Data Analytics Boot Camp Course Work/Boot Camp/Module 15 - Statistics and R/Application/Module 15 Challenge/mechacar-statistical-analysis/assets/data/suspension_coil.csv")                                                           # D2:Step 2
+
+# View the head of the created DataFrame
+head(suspension_coil)
+
+#create summary of Mean, Median, Variance, and standard deviation
+total_summary <- suspension_coil %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+head(total_summary)
+
+#Calculate total samples
+total_samples  <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarise(number_of_samples = n())
+head(total_samples)
+
+#Create a DataFrame and group each manufacturing lot by the mean, median, variance, and standard deviation
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+head(lot_summary)
+
+# Deliverable 3: T-Tests on Suspension Coils
+# Comparing all lot sample vs the population
+# t-Test on the suspension_coil PSI data
+t.test(suspension_coil$PSI,mu=1500) 
+
+# One Sample t-Test on Lot 1
+suspension_coil_Lot1 <-subset(suspension_coil,Manufacturing_Lot == 'Lot1')
+t.test(suspension_coil_Lot1$PSI,mu=1500) 
+
+# One Sample t-Test on Lot 2
+suspension_coil_Lot2 <-subset(suspension_coil,Manufacturing_Lot == 'Lot2')
+t.test(suspension_coil_Lot2$PSI,mu=1500) 
+
+# One Sample t-Test on Lot 3
+suspension_coil_Lot3 <-subset(suspension_coil,Manufacturing_Lot == 'Lot3')
+t.test(suspension_coil_Lot3$PSI,mu=1500) 
